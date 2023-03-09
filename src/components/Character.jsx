c9d4f56457647d9eaa27f53bc1d9127ff2c9408b
Flavio Oliveira <61277545+fl4viooliveira@users.noreply.github.com>
2023-03-09 16:42:20 +0000

    New app

---
+++ src/components/Character.jsx
@@ -0,0 +1,19 @@
+function Character(props) {
+    return (
+        <div data-test="component-character">
+            <h1 data-test="char-name">{props.name}</h1>
+            <img src={props.imgUrl} data-test="char-img" alt="test"></img>
+            <ul data-test="char-list">
+                <li data-test="char-birth">Date of Birth: {props.birth}</li>
+                <li data-test="char-death">Date of Death: {props.death}</li>
+                <li data-test="char-race">Race: {props.race}</li>
+                <li data-test="char-realm">Realm: {props.realm}</li>
+                <li data-test="char-spouse">Spouse: {props.spouse}</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Character;
+
+
